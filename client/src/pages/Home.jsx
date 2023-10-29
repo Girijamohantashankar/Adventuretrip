070e3ce92fe5deacbfc8b1da32d5ebb98f49d8ce
Girijajethitech <girija.shankar@jethitech.com>
2023-10-29 12:28:07 +0530

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -106,7 +106,7 @@ export default function Home() {
           <div className=''>
             <div className='my-3'>
               <h2 className='text-2xl font-semibold text-slate-600'>Recent places for rent</h2>
-              <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=rent'}>Show more places for rent</Link>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=rent'}>Show more places for Trip</Link>
             </div>
             <div className='flex flex-wrap gap-4'>
               {rentListings.map((listing) => (
