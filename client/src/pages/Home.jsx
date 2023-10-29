c6774e09b8390148c7c372e1f37fd6f16da02b07
Girijajethitech <girija.shankar@jethitech.com>
2023-10-29 12:51:59 +0530

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -105,7 +105,7 @@ export default function Home() {
         {rentListings && rentListings.length > 0 && (
           <div className=''>
             <div className='my-3'>
-              <h2 className='text-2xl font-semibold text-slate-600'>Recent places for rent</h2>
+              <h2 className='text-2xl font-semibold text-slate-600'>Recent places for Trip</h2>
               <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=rent'}>Show more places for Trip</Link>
             </div>
             <div className='flex flex-wrap gap-4'>
@@ -118,7 +118,7 @@ export default function Home() {
         {saleListings && saleListings.length > 0 && (
           <div className=''>
             <div className='my-3'>
-              <h2 className='text-2xl font-semibold text-slate-600'>Recent places</h2>
+              <h2 className='text-2xl font-semibold text-slate-600'>Recent places for Trip</h2>
               <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=sale'}>Show more places for Trip</Link>
             </div>
             <div className='flex flex-wrap gap-4'>
